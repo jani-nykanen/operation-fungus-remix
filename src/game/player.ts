67e650174f6fa3c7f57e59b7c3369183db058180
jani-nykanen <jnyknn@gmail.com>
2020-01-15 19:24:56 +0200

    Refactoring

--- src/game/player.ts
+++ src/game/player.ts
@@ -72,6 +72,42 @@ class PlayerAI extends AIComponent {
 }
 
 
+// Player render component
+class PlayerRenderComponent extends RenderComponent {
+
+
+    private sprHead : Sprite;
+
+
+    constructor(base : EntityBase) {
+
+        super(base, 32, 32);
+
+        // Create additional sprites
+        this.sprHead = new Sprite(32, 16);
+        this.sprHead.setFrame(0, 1);
+    }
+
+
+    // Override draw
+    public draw(c : Canvas, bmp? : Bitmap) {
+
+        let x = Math.round(this.base.pos.x - this.spr.width/2);
+        let y = Math.round(this.base.pos.y - this.spr.width/2);
+
+        // Draw body
+        c.drawSprite(this.spr, 
+            c.getBitmap("player"),
+            x, y, this.flip);
+
+        // Draw head
+        c.drawSprite(this.sprHead,
+            c.getBitmap("player"),
+            x, y, this.flip);
+    }
+}
+
+
 // The player itself
 class Player extends Entity {
 
@@ -79,7 +115,7 @@ class Player extends Entity {
 
         super(x, y);
 
-        this.renderComp = new RenderComponent(this.base, 32, 32);
+        this.renderComp = new PlayerRenderComponent(this.base);
         this.ai = new PlayerAI(this.base);
 
         this.base.acc.x = 0.25;
