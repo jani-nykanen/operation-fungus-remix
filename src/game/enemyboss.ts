75e26995e79c7406270bcec96e5d6902ef5498f2
jani-nykanen <jnyknn@gmail.com>
2020-01-31 16:52:20 +0200

    Meh

--- src/game/enemyboss.ts
+++ src/game/enemyboss.ts
@@ -5,3 +5,18 @@
 
 // (This file has a weird name so that it can extend
 // the Enemy class)
+
+
+class BossAI extends AIComponent {
+
+}
+
+
+class BossRenderer extends RenderComponent {
+
+}
+
+
+class Boss extends Entity {
+
+}
